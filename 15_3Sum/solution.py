e175d559abee6fa54dd7ddefadff0bea70cfd4f2
isidor99 <sajinovic.isidor99@gmail.com>
2024-01-15 14:52:42 +0100

    Task 15: Added solution using two pointers aproach

--- 15_3Sum/solution.py
+++ 15_3Sum/solution.py
@@ -4,4 +4,47 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        pass
+        # Create result array
+        res = []
+        
+        # Sort array
+        nums.sort()
+        
+        # Loop
+        for i in range(0, len(nums) - 2):
+            # Get rid off duplicates with this condition
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            
+            # Create "two pointers"
+            j = i + 1
+            k = len(nums) - 1
+            target = -nums[i]
+
+            # Loop while j is less than k            
+            while j < k:
+                # Sum of element at j and k positions
+                sum = nums[j] + nums[k]
+                
+                # Is sum is equal to target, triple was found
+                if sum == target:
+                    res.append([nums[i], nums[j], nums[k]])
+                    
+                    # Skip same numbers
+                    while j < k and nums[j] == nums[j + 1]:
+                        j = j + 1
+                    
+                    while j < k and nums[k] == nums[k - 1]:
+                        k = k - 1
+                        
+                    # Finally, increment/decrement "pointers"
+                    j = j + 1
+                    k = k - 1
+                
+                elif sum < target:
+                    # If sum is less that target, increment j, else decrement k
+                    j = j + 1
+                else:
+                    k = k - 1
+                        
+        return res
